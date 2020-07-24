68e0d81b0ca6c1e2b12ecebedc967cebea8baf8b
Matthew Carbone <x94carbone@gmail.com>
2020-07-24 14:47:08 -0400

    Update tests for csv loaders

--- crescendo/loaders/base.py
+++ crescendo/loaders/base.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+from crescendo.utils.logger import logger_default as dlog
+
 
 class _CrescendoBaseDataLoader:
 
-    def __init__(self, *, debug=False):
+    def __init__(self, *, debug=False, data_kind='unknown'):
         """Base initializer.
 
         Parameters
@@ -12,12 +14,32 @@ class _CrescendoBaseDataLoader:
             If set to -1 (default) then we're not in debug mode. If some
             integer > 0, then that is the number of total data points loaded
             in to self.raw.
+        data_kind : {'features', 'targets', 'all', 'meta'}
+            The type of data being loaded. This is critical as the label will
+            be used downstream for model compatibility.
+            * 'features': the loaded csv corresponds to the features of the
+                data.
+            * 'targets': the loaded csv corresponds to the targets of the data.
+            * 'all': the loaded csv contains all data (features and targets
+                in different columns).
+            * 'meta': the loaded csv contains extra data that is neither the
+                features nor targets.
+            * 'id': an optional "column" that identifies the trial by some
+                label other than the index (row).
         """
 
         self.path = None
         self.raw = None
-        self.data_kind = None
         self.debug = debug
+
+        dlog.info(f"Initializng Loader for {data_kind} data")
+        if data_kind not in ['features', 'targets', 'all', 'meta', 'id']:
+            critical = \
+                f"Argument 'data_kind' {data_kind} not valid. Choices " \
+                f"are 'features', 'targets', 'all', 'meta', 'id'."
+            dlog.critical(critical)
+            raise RuntimeError(critical)
+        self.data_kind = data_kind
 
     def load(self):
         raise NotImplementedError
--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import numpy as np
 import pytest
 
 from crescendo.loaders.base import _CrescendoBaseDataLoader
@@ -10,7 +9,11 @@ from crescendo.loaders.text_loaders import CSVLoader
 class TestBaseLoader:
 
     def test_base_init(self):
-        _ = _CrescendoBaseDataLoader()
+        _ = _CrescendoBaseDataLoader(data_kind='all')
+        _ = _CrescendoBaseDataLoader(data_kind='features')
+        _ = _CrescendoBaseDataLoader(data_kind='targets')
+        _ = _CrescendoBaseDataLoader(data_kind='meta')
+        _ = _CrescendoBaseDataLoader(data_kind='id')
 
 
 class TestCSVLoader:
@@ -18,33 +21,33 @@ class TestCSVLoader:
     def test_csv_load(self):
         """Ensures that the testing csv files are loaded properly."""
 
-        ds = CSVLoader()
-        ds.load("data/test_3_column.csv", data_kind='all')
+        ds = CSVLoader(data_kind='all')
+        ds.load("data/test_3_column.csv")
         assert ds.raw.shape[0] == 1000
         assert ds.raw.shape[1] == 3
 
-        ds = CSVLoader()
-        ds.load("data/test_10_column.csv", data_kind='features')
+        ds = CSVLoader(data_kind='features')
+        ds.load("data/test_10_column.csv")
         assert ds.raw.shape[0] == 1000
         assert ds.raw.shape[1] == 10
 
-        ds = CSVLoader()
         with pytest.raises(Exception):
-            ds.load("data/test_10_column.csv", data_kind='incorrect_data_kind')
+            ds = CSVLoader(data_kind='incorrect_data_kind')
 
     def test_get(self):
         """Tests the get method."""
 
-        ds = CSVLoader()
-        ds.load("data/test_10_column.csv", data_kind='features')
+        ds = CSVLoader(data_kind='features')
+        ds.load("data/test_10_column.csv")
         features = ds.get('features', [0, 1, 2])
         assert features.raw.shape[0] == 1000
         assert features.raw.shape[1] == 3
 
-        ds = CSVLoader()
-        ds.load("data/test_10_column.csv", data_kind='features')
+        ds = CSVLoader(data_kind='features')
+        ds.load("data/test_10_column.csv")
         _ = ds.get('features', [0, 1, 2])
         _ = ds.get('targets', [0, 1, 2])
+        _ = ds.get('meta', [0, 1, 2])
         _ = ds.get('meta', [0, 1, 2])
         with pytest.raises(Exception):
             _ = ds.get('incorrect_data_kind', [0, 1, 2])
@@ -52,8 +55,8 @@ class TestCSVLoader:
     def test_assert_integrity(self):
         """"""
 
-        ds = CSVLoader()
-        ds.load("data/test_10_column.csv", data_kind='features')
+        ds = CSVLoader(data_kind='features')
+        ds.load("data/test_10_column.csv")
         ds.assert_integrity(raise_error=True)
         features = ds.get(
             'features', ['feature_1', 'feature_2', 'feature_7', 'feature_9']
