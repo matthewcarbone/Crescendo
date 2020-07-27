430914858a07ca1cbb7ad1693ee8e3843c42304b
Matthew Carbone <x94carbone@gmail.com>
2020-07-27 09:21:38 -0400

    Rewrite the csv loader
    Pandas already handles ingesting csv files and there is no reason to
    duplicate those efforts. Thus, the loaders will be changed to
    Dataset objects that contain raw data, and eventually features and
    targets, which is later processed into torch-compatible format.

--- crescendo/loaders/base.py
+++ crescendo/loaders/base.py
@@ -5,7 +5,7 @@ from crescendo.utils.logger import logger_default as dlog
 
 class _CrescendoBaseDataLoader:
 
-    def __init__(self, *, debug=-1, data_kind='unknown'):
+    def __init__(self, debug=-1, raise_error=False):
         """Base initializer.
 
         Parameters
@@ -13,24 +13,15 @@ class _CrescendoBaseDataLoader:
         debug : int
             If set to -1 (default) then we're not in debug mode. If some
             integer > 0, then that is the number of total data points loaded
-            in to self.raw.
-        data_kind : {'features', 'targets', 'all', 'meta'}
-            The type of data being loaded. This is critical as the label will
-            be used downstream for model compatibility.
-            * 'features': the loaded csv corresponds to the features of the
-                data.
-            * 'targets': the loaded csv corresponds to the targets of the data.
-            * 'all': the loaded csv contains all data (features and targets
-                in different columns).
-            * 'meta': the loaded csv contains extra data that is neither the
-                features nor targets.
-            * 'id': an optional "column" that identifies the trial by some
-                label other than the index (row).
+            into self.raw.
+        raise_error : bool
+            If true, will raise errors and terminate the program on non
+            critical errors
         """
 
         self.raw = None
         self.debug = debug
-        self.data_kind = data_kind
+        self.raise_error = raise_error
 
     @property
     def debug(self):
@@ -44,26 +35,22 @@ class _CrescendoBaseDataLoader:
         self._debug = d
 
     @property
-    def data_kind(self):
-        return self._data_kind
+    def raise_error(self):
+        return self._raise_error
 
-    @data_kind.setter
-    def data_kind(self, data_kind):
-        assert isinstance(data_kind, str)
-        if data_kind not in ['features', 'targets', 'all', 'meta', 'id']:
-            critical = \
-                f"Argument 'data_kind' {data_kind} not valid. Choices " \
-                f"are 'features', 'targets', 'all', 'meta', 'id'."
-            dlog.critical(critical)
-            raise RuntimeError(critical)
-        dlog.info(f"Initializing Loader for {data_kind} data")
-        self._data_kind = data_kind
+    @raise_error.setter
+    def raise_error(self, r):
+        assert isinstance(r, bool)
+        self._raise_error = r
 
     def load(self):
+        """Optional, depending on the loader type."""
+
         raise NotImplementedError
 
-    def get(self):
-        raise NotImplementedError
+    def init_raw(self):
+        """Initializes the raw attribute from the features and targets. Takes
+        optional arguments depending on the data type, possibly on how to
+        construct the features and targets themselves."""
 
-    def assert_integrity(self):
         raise NotImplementedError
