71056b7bd8f1264c6c0dbb2e696912ce7e454398
Matthew Carbone <x94carbone@gmail.com>
2020-09-01 17:47:44 -0400

    Update and add documentation to Meter
    * Also move the configuration templates to a config directory

--- crescendo/utils/ml_utils.py
+++ crescendo/utils/ml_utils.py
@@ -6,10 +6,8 @@ import torch
 
 from crescendo.utils.logger import logger_default as dlog
 
-CUDA_AVAIL = torch.cuda.is_available()
 
-
-def init_cuda(arg, cuda_avail=CUDA_AVAIL):
+def init_cuda(arg, cuda_avail=torch.cuda.is_available()):
     """This is a critical helper function that allows seamless swapping
     between CPUs and GPUs. If CUDA is available, this function will apply the
     cuda() method to the object, essentially sending it to the GPU. If CUDA
@@ -45,7 +43,21 @@ def epoch_time(start_time, end_time):
 def mean_and_std(dat):
     """Takes a list of N data points, each with M features, and computes the
     mean and spread over the data points. Also logs the average mean and
-    average spread over all the features."""
+    average spread over all the features.
+
+    Parameters
+    ----------
+    dat : list
+        The list of data to be turned into a numpy array. Contains training
+        examples at the first level of depth, and features at the second level
+        of depth.
+
+    Returns
+    -------
+    np.ndarray, np.ndarray
+        The mean and standard deviations of the features, averaged over all
+        examples in dat.
+    """
 
     dat = np.array(dat)
     mean = dat.mean(axis=0)
@@ -59,7 +71,18 @@ def mean_and_std(dat):
 
 class Meter:
     """Used for keeping track of losses and other information that needs to
-    be cached as the model trains."""
+    be cached as the model trains.
+
+    Attributes
+    ----------
+    root : str
+        The location to which to save the results as produced by Meter. For
+        example, training information will be saved to root/info.txt.
+    fname : str
+        The file name for saving information, e.g. root/info.txt.
+    train_loss_list, valid_loss_list, learning_rates : list
+        Lists containing various training information.
+    """
 
     def __init__(self, root):
         self.root = root
@@ -69,17 +92,28 @@ class Meter:
         self.learning_rates = []
 
     def step(self, epoch, t_loss, v_loss, lr, elapsed):
-        """Writes the losses to disk and logs the information."""
+        """Writes the losses to disk and logs the information - i.e. steps the
+        Meter.
 
-        with open(self.fname, "w") as f:
+        Parameters
+        ----------
+        epoch : int
+            The current epoch.
+        t_loss, v_loss, lr, elapsed : float
+            Properties of training to be saved to disk: the training loss,
+            validation loss, current learning rate, and time elapsed for
+            training on that epoch.
+        """
+
+        with open(self.fname, "a") as f:
             f.write(
-                f"{epoch}\t{t_loss:.02e}\t{v_loss:.02e}\t{lr:.02e}\t"
+                f"{epoch:05}\t{t_loss:.04e}\t{v_loss:.04e}\t{lr:.04e}\t"
                 f"{elapsed:.02f}\n"
             )
 
-        dlog.info(f"Epoch {epoch:04} [{elapsed:3.02f}s]")
-        dlog.info(f'\tTrain Loss: {t_loss:.02e}')
-        dlog.info(f"Eval complete: loss {v_loss:.02e}")
+        dlog.info(f"Epoch {epoch:05} [{elapsed:3.02f}s]")
+        dlog.info(f'\tTrain Loss: {t_loss:.03e}')
+        dlog.info(f"\tValid loss {v_loss:.03e}")
 
         self.train_loss_list.append(t_loss)
         self.valid_loss_list.append(v_loss)
