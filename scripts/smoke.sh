68f84d04daf736e6399e5e63c4471f596602a9bc
Matthew Carbone <x94carbone@gmail.com>
2023-04-24 12:55:07 -0400

    Add hp tuning smoke test

--- scripts/smoke.sh
+++ scripts/smoke.sh
@@ -6,3 +6,4 @@ cr model=mlp data=california_housing debug=default
 cr model=mlp data=california_housing debug=fdr
 cr model=mlp data=california_housing debug=limit
 cr model=mlp data=california_housing debug=overfit
+cr model=mlp data=california_housing hparams_search=optuna
