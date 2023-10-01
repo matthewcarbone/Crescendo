8933aa723da2db39c08446bb8407a3599fc80614
Matthew Carbone <x94carbone@gmail.com>
2023-10-01 19:29:57 -0400

    Debug

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -6,7 +6,7 @@ replace_version_in_init () {
     pip install dunamai~=1.12
     version="$(dunamai from git --style pep440 --no-metadata)"
     dunamai check "$version" --style pep440
-    sed_command="s/'dev'  # semantic-version-placeholder/'$version'/g"
+    sed_command="s/'v0.0.0'  # semantic-version-placeholder/'$version'/g"
     if [[ "$OSTYPE" == "darwin"* ]]; then
         sed -i '' "$sed_command" "$PACKAGE_NAME"/__init__.py
     else
@@ -18,7 +18,7 @@ replace_version_in_init () {
 
 reset_version_to_dev () {
     current_version=$(grep "__version__" "$PACKAGE_NAME"/__init__.py)
-    sed_command="s/$current_version/__version__ = 'dev'  # semantic-version-placeholder/g"
+    sed_command="s/$current_version/__version__ = 'v0.0.0'  # semantic-version-placeholder/g"
     if [[ "$OSTYPE" == "darwin"* ]]; then
         sed -i '' "$sed_command" "$PACKAGE_NAME"/__init__.py
     else
